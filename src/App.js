9a5f7465d241e3cdcef5a9f037a54eb91dba0b11
Shadow <shadowdemonics@gmail.com>
2022-10-19 22:46:06 +0500

    GG
    Test

---
+++ src/App.js
@@ -0,0 +1,25 @@
+//taking input from use as variable
+var oprerator = prompt("Operator: ")
+var num1 = prompt("Number 1:")
+var num2 = prompt("Number 2:")
+ 
+//using if else for selection of operator
+if(oprerator == "*")
+{
+console.log ( num1 * num2)
+}
+  else if(oprerator == "+")
+
+  
+{
+console.log ( num1 + num2)
+}
+else if(oprerator == "-")
+{
+console.log ( num1 - num2)
+
+}
+else if(oprerator == "/")
+{
+console.log ( num1 / num2)
+}
